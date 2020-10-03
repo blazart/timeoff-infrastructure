e673a5b4b6ffb7f91bc2dc69ff0d859c5940c4ef
Fabian Novoa <fabianovoa@seven4n.com>
2020-10-02 23:01:31 -0500

    Removing illegal character

--- application/autoscaling.tf
+++ application/autoscaling.tf
@@ -1,7 +1,7 @@
 resource "aws_appautoscaling_target" "ecs_target" {
   max_capacity = var.max_capacity
   min_capacity = var.min_capacity
-  resource_id = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.main.name}}"
+  resource_id = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.main.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace = "ecs"
   depends_on = [
